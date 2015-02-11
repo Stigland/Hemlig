0e6cc1900b1dd4c90c6529ef5fc3d9ba9bcdde3b
Stigland <gusstied@student.gu.se>
2015-02-11 15:31:02 +0100

    rättnings-synk

---
+++ edu/sysvp/program/Menu.java
@@ -0,0 +1,62 @@
+package edu.sysvp.program;
+
+import com.sandklef.edu.ConsoleMenu.*;
+
+//class MenuItemZero implements MenuItem{
+		//public void menuItemSelected(ConsoleMenuEvent e) { 
+		    //System.out.println("Egen menuitem");  
+	    //}
+//}
+public class Menu {
+    
+    ClubHelper ch ;
+    
+    public Menu(ClubHelper c) {
+	ch = c;
+    }
+
+    public void run() {
+
+   	ConsoleMenu menu = new ConsoleMenu("-----===< Club Manager >===-----");
+
+	menu.addMenuItem(new MenuItem(){
+		public void menuItemSelected(ConsoleMenuEvent e) { 
+		    ch.printMembersAlpha(); } 
+	    },"List all members by family name" );
+
+	menu.addMenuItem(new MenuItem(){
+		public void menuItemSelected(ConsoleMenuEvent e) { 
+		    ch.printMembersAlphaFirstName(); } 
+	    },"List all members by first name" );
+
+	menu.addMenuItem(new MenuItem(){
+		public void menuItemSelected(ConsoleMenuEvent e) { 
+		    ch.printMembersAlpha(); } 
+	    },"List all members by ID" );
+
+	menu.addMenuItem(new MenuItem(){
+		public void menuItemSelected(ConsoleMenuEvent e) { 
+		    ch.printMembersAlpha(); } 
+	    },"List all members by team" );
+
+	menu.addMenuItem(new MenuItem(){
+		public void menuItemSelected(ConsoleMenuEvent e) { 
+		    ch.printMembersAlpha(); } 
+	    },"List all members of a specific team" );
+
+	menu.addMenuItem(new MenuItem(){
+		public void menuItemSelected(ConsoleMenuEvent e) { 
+		    ch.printMembersAlpha(); } 
+	    },"List all parents of a specific team" );
+
+	menu.addMenuItem(new MenuItem(){
+		public void menuItemSelected(ConsoleMenuEvent e) { 
+		    ch.printMembersAlpha(); } 
+	    },"List all e-mail adresses of a member" );
+
+
+
+	menu.run();
+    }
+
+}
